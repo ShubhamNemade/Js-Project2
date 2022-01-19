f87bac7ff62b5c7976d8a6d3903acaf0928faca1
shubhamnemade114@gmail.com <shubhampatilpra@gmail.com>
2022-01-19 08:32:41 +0530

    COUNTER (Inc-Dec)

---
+++ Counter(Incr-Decr)/counter.js
@@ -0,0 +1,19 @@
+let increment = document.querySelector("#increment");
+let decrement = document.querySelector("#decrement");
+let countervalue = document.querySelector("#countervalue");
+
+let counter = 0;
+increment.addEventListener("click",function(){
+    counter++;
+    countervalue.innerText = counter;
+});
+
+decrement.addEventListener("click",function(){
+    counter--;
+    countervalue.innerText = counter;
+});
+
+
+
+
+
